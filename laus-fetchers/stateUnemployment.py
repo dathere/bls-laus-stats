7e2a02783c9a364c22113648fbbf280907ad0af8
Abdur Rahman <mabdurrahman303@gmail.com>
2025-08-26 23:17:59 +0530

    Change start year from 2025 to 2000 in payload

--- laus-fetchers/stateUnemployment.py
+++ laus-fetchers/stateUnemployment.py
@@ -12,7 +12,7 @@ def get_data(place_series_ids, df):
     
     payload = {
         "seriesid": place_series_ids,
-        "startyear": "2025",
+        "startyear": "2000",
         "endyear": "2025",
         "annualaverage": True,
         "catalog": True,
